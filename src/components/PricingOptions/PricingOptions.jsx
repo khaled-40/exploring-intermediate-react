70690ee88944c7c314f23541d8fd546f638b34ce
khaled-40 <khaledmasud.dev@gmail.com>
2025-10-01 12:25:14 +0600

    added daisy pricing card by removing the previous card

--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -1,15 +1,20 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricingCard from '../DaisyCard/DaisyPricingCard';
 
 const PricingOptions = ({ pricingPromise }) => {
     const pricingData = use(pricingPromise);
     return (
         <div>
-            <h2 className='font-bold text-2xl'>Our Pricing</h2>
-            <div className='flex gap-8 justify-between'>
-                {
+            <h2 className='font-bold text-2xl my-8'>Get Our Membership</h2>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-8'>
+                {/* {
                     pricingData.map(pricingCard => <PricingCard key={pricingCard.id} pricingCard={pricingCard}></PricingCard>)
+                } */}
+                {
+                    pricingData.map(pricing => <DaisyPricingCard key={pricing.id} pricing={pricing}></DaisyPricingCard>)
                 }
+
             </div>
         </div>
     );
